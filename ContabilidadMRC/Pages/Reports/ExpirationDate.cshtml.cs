de850e6733c4fbc2eb89852171389843eb4271a9
Fredy Cortés <fred.cor.14@gmail.com>
2020-06-19 01:28:07 -0500

    Creación de reporte 2
    productos proximos a vencer

--- ContabilidadMRC/Pages/Reports/ExpirationDate.cshtml.cs
+++ ContabilidadMRC/Pages/Reports/ExpirationDate.cshtml.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContabilidadMRC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContabilidadMRC.Pages.Reports
 {
     public class ExpirationDateModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ContabilidadMRC.Data.ContabilidadMRCContext _context;
 
+        public ExpirationDateModel(ContabilidadMRC.Data.ContabilidadMRCContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            DateTime expirationDate = DateTime.Now.AddDays(15);
+            Product = await _context.Product
+                .Where(p => p.ExpiredAt <= expirationDate)
+                .Include(p => p.TypeProduct)
+                .Include(p => p.Brand)
+                .ToListAsync();
         }
     }
 }
