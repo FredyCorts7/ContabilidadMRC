51a29f463786abbf5ff73c88718626a30c6e5c87
Fredy Cortés <fred.cor.14@gmail.com>
2020-06-19 00:57:31 -0500

    Creación de reporte 1
    productos agotados

--- ContabilidadMRC/Pages/Reports/StockSoldOut.cshtml.cs
+++ ContabilidadMRC/Pages/Reports/StockSoldOut.cshtml.cs
@@ -2,16 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContabilidadMRC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContabilidadMRC.Pages.Reports
 {
     public class StockSoldOutModel : PageModel
     {
-        public void OnGet()
-        {
 
+        private readonly ContabilidadMRC.Data.ContabilidadMRCContext _context;
+
+        public StockSoldOutModel(ContabilidadMRC.Data.ContabilidadMRCContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Product
+                .Where(p => p.Stock <= 5)
+                .Include(p => p.TypeProduct)
+                .Include(p => p.Brand)
+                .ToListAsync();
         }
     }
 }
