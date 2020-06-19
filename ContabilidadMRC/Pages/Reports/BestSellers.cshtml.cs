c443eb8d83eb0c88b7816381c5bb29829c710afd
Fredy Cortés <fred.cor.14@gmail.com>
2020-06-18 23:50:34 -0500

    Creando pagina de reportes
    productos proximos a vencer
    productos agotados
    productos mas vendidos
    ingresos por fechas

---
+++ ContabilidadMRC/Pages/Reports/BestSellers.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContabilidadMRC.Pages.Reports
+{
+    public class BestSellersModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
